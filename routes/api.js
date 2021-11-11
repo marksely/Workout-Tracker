3dff0369357f2d08e16da7493afebfbce580892b
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-11-10 19:40:20 -0500

    added code to connection for database

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,48 @@
 const router = require("express").Router();
 const {Workout} = require("../models/workouts");
-const path = require('path');
 
 router.get('/api/workouts',(req,res) => {
     Workout.find({})
+    .sort({ date: -1 })
     .then(data => {
         res.json(data);
+        console.log(data);
     }) 
     .catch(err => {
         res.status(400).json(err);
     })
 });
 
-router.get('/api/stats', (req,res) => {
-    Workout.find({})
+router.put('/api/workouts/:id', (req,res) => {
+    Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $push: { exercises: req.body}
+        }
+    )
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+});
+
+router.post('/api/workouts', ({body},res) => {
+    console.log(body);
+    Workout.create(body)
     .then(data => {
         res.json(data);
     })
     .catch(err => {
         res.status(400).json(err);
+        console.log(err);
     })
+});
+
+router.get('/api/workouts/range', (req,res) => {
 });
  
 
--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 app.use(require("./routes/api"));
